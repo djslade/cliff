f2e090f94ca6a96bd3c83f66243af33b19ad85d4
fenroe <scrambles9494@gmail.com>
2025-02-28 17:26:15 +0200

    added more handlers and restricted index route

--- main.go
+++ main.go
@@ -9,24 +9,34 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Cliff"))
 }
 
+func snipperView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet..."))
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/snippet/view", snipperView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("starting server on :4000")
 
 	log.Fatal(http.ListenAndServe(":4000", mux))
 
 	/*
-	http.ListenAndServe()
-	addr parameter expects format 'host:port'
-	When the host portion is omitted the function will listen on the machine's
-	available network interfaces. That's mostly fine unless the device has more 
-	than one interface and you only want to listen to one of them.
+		http.ListenAndServe()
+		addr parameter expects format 'host:port'
+		When the host portion is omitted the function will listen on the machine's
+		available network interfaces. That's mostly fine unless the device has more
+		than one interface and you only want to listen to one of them.
 
-	The port doesn't have to be a number. In some cases you may see a project or
-	piece of documentation use something like ':http' instead. If that's the case, 
-	this function will look up the relevant port number from your /etc/services file, 
-	and will throw an error if unable to find a match. 
+		The port doesn't have to be a number. In some cases you may see a project or
+		piece of documentation use something like ':http' instead. If that's the case,
+		this function will look up the relevant port number from your /etc/services file,
+		and will throw an error if unable to find a match.
 	*/
 }
