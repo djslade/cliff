d3eca3a968df0c782b461457af21a9dab9710275
fenroe <scrambles9494@gmail.com>
2025-03-01 17:53:47 +0200

    added latest method to snippets model

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,27 +5,36 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"text/template"
 
 	"github.com/djslade/cliff/internal/models"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
-	files := []string{
-		"./ui/html/base.tmpl.html",
-		"./ui/html/partials/nav.tmpl.html",
-		"./ui/html/pages/home.tmpl.html",
-	}
-	ts, err := template.ParseFiles(files...)
+	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, r, err)
 		return
 	}
-	if err := ts.ExecuteTemplate(w, "base", nil); err != nil {
-		app.serverError(w, r, err)
-		return
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%+v\n", snippet)
 	}
+	/*
+		files := []string{
+			"./ui/html/base.tmpl.html",
+			"./ui/html/partials/nav.tmpl.html",
+			"./ui/html/pages/home.tmpl.html",
+		}
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			app.serverError(w, r, err)
+			return
+		}
+		if err := ts.ExecuteTemplate(w, "base", nil); err != nil {
+			app.serverError(w, r, err)
+			return
+		}
+	*/
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
