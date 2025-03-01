93339e43ef50c9820683a0c60c5a241983f58e64
fenroe <scrambles9494@gmail.com>
2025-03-01 15:56:50 +0200

    added snippets model

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,12 +7,14 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/djslade/cliff/internal/models"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/subosito/gotenv"
 )
 
 type application struct {
-	logger *slog.Logger
+	logger   *slog.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
@@ -32,7 +34,8 @@ func main() {
 	defer db.Close()
 
 	app := &application{
-		logger: logger,
+		logger:   logger,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	app.logger.Info("starting server", slog.String("addr", *addr))
