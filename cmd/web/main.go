8bc6de32bd46860da8a6b003a9be128b52924161
fenroe <scrambles9494@gmail.com>
2025-03-02 19:54:23 +0200

    configured server struct to use structured logger for error logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -61,29 +61,17 @@ func main() {
 		sessionManager: sessionManager,
 	}
 
-	app.logger.Info("starting server", slog.String("addr", *addr))
-
 	srv := &http.Server{
-		Addr:    *addr,
-		Handler: app.routes(),
+		Addr:     *addr,
+		Handler:  app.routes(),
+		ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
 	}
+
+	app.logger.Info("starting server", slog.String("addr", *addr))
 
 	err = srv.ListenAndServe()
 	app.logger.Error(err.Error())
 	os.Exit(1)
-
-	/*
-		http.ListenAndServe()
-		addr parameter expects format 'host:port'
-		When the host portion is omitted the function will listen on the machine's
-		available network interfaces. That's mostly fine unless the device has more
-		than one interface and you only want to listen to one of them.
-
-		The port doesn't have to be a number. In some cases you may see a project or
-		piece of documentation use something like ':http' instead. If that's the case,
-		this function will look up the relevant port number from your /etc/services file,
-		and will throw an error if unable to find a match.
-	*/
 }
 
 func openDB(dsm string) (*sql.DB, error) {
