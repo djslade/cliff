6115e79f180ac7061871089d46460f899eedb51b
fenroe <scrambles9494@gmail.com>
2025-03-02 17:20:54 +0200

    abstracted validation logic into a validator package

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,17 +5,16 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"strings"
-	"unicode/utf8"
 
 	"github.com/djslade/cliff/internal/models"
+	"github.com/djslade/cliff/internal/validator"
 )
 
 type snippetCreateForm struct {
-	Title       string
-	Content     string
-	Expires     int
-	FieldErrors map[string]string
+	Title   string
+	Content string
+	Expires int
+	validator.Validator
 }
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -75,28 +74,20 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 
 	form.FieldErrors = make(map[string]string)
 
-	if strings.TrimSpace(form.Title) == "" {
-		form.FieldErrors["title"] = "This field cannot be blank"
-	} else if utf8.RuneCountInString(form.Title) > 100 {
-		form.FieldErrors["title"] = "This field cannot be more than 100 characters long"
-	}
+	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
+	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters long")
 
-	if strings.TrimSpace(form.Content) == "" {
-		form.FieldErrors["content"] = "This field cannot be blank"
-	}
+	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
 
 	// First convert to int. Conversion can throw an error, so check for that first
 	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
-	if err != nil {
-		form.FieldErrors["expires"] = "This field must equal 1, 7 or 365"
-	}
-	// Now assign the expires value to the form struct and validate normally
+	form.CheckField(err != nil, "expires", "This field must be a valid integer")
+	// Now safe to assign expires to form and proceed if normal
+	// This is safe as, if error is not nil, expires will be 0.
 	form.Expires = expires
-	if expires != 1 && expires != 7 && expires != 365 {
-		form.FieldErrors["expires"] = "This field must equal 1, 7 or 365"
-	}
+	form.CheckField(validator.PermittedValue(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7 or 365")
 
-	if len(form.FieldErrors) > 0 {
+	if !form.Valid() {
 		data := app.newTemplateData(r)
 		data.Form = form
 		app.render(w, r, http.StatusUnprocessableEntity, "create.tmpl.html", data)
